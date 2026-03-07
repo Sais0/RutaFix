603bfb00d355b9d609761459d049cecec29cd241
Sais0 <sbabativacastro@gmail.com>
2026-03-07 17:06:29 -0500

    Agregamos el Login, y aplicamos cambios en los demas Layout´s para que la app funcione, app en perfecto estado con Splash, Home y Login, todo en LinearLayout.

--- app/src/main/java/com/example/taller2/ui/Home.kt
+++ app/src/main/java/com/example/taller2/ui/Home.kt
@@ -3,10 +3,19 @@ package com.example.taller2.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taller2.R
+import android.widget.Button
+import android.content.Intent
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val btnComienza = findViewById<Button>(R.id.btnComienza)
+
+        //Le decimos qué hacer al hacer clic
+        btnComienza.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
